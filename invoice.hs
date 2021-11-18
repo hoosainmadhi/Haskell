0dff048e0c90af6fe93a27a2b68868be79aac847
hoosain <hoosain.madhi@gmail.com>
2021-11-18 09:37:15 +0000

    invoice

--- invoice.hs
+++ invoice.hs
@@ -5,14 +5,38 @@ import Data.Time.Clock
 data CustomerInvoice = CustomerInvoice
     {
      invoiceNumber    :: Int
-     , amountDue      :: Dollars
-     , tax            :: Dollars 
+     , amountDue      :: ZAR
+     , tax            :: ZAR 
      , billableItems  :: [String]
      , status         :: InvoiceStatus 
-     , createdAt      :: UTCTime
+    --  , createdAt      :: UTCTime
      , dueDate        :: Day 
-    }
+    } 
 
 data InvoiceStatus = Issued | Paid | Cancelled
 
-type Dollars =  Int
+type ZAR =  Int
+
+invoice = CustomerInvoice
+        {
+            invoiceNumber=1
+            ,amountDue = 200
+            ,tax = 10
+            ,billableItems=["design","programming"]
+            ,status=Cancelled
+            -- ,createdAt=currentTime
+            ,dueDate= fromGregorian 2022 2 15
+        }
+
+
+createCustomerNotification :: CustomerInvoice -> String
+createCustomerNotification invoice =
+    case status invoice of
+        Issued ->
+            "Invoice #" ++ show (invoiceNumber invoice) ++ " due on " ++ show (dueDate invoice)
+
+        Paid ->
+            "Successfully paid invoice #" ++ show (invoiceNumber invoice)
+
+        Cancelled ->
+            "Invoice #" ++ show (invoiceNumber invoice) ++ " has been canceled"
