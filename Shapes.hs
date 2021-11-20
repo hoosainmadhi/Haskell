4896a04db21fe0b74ed3b26cce3a9d4439c8305d
Hoosain Madhi <hoosain.madhi@gmail.com>
2021-11-20 08:22:16 +0200

    diagram.io

--- Shapes.hs
+++ Shapes.hs
@@ -1,17 +1,35 @@
-type Point = (Int, Int)
+type Point = (Float, Float)
+
+type Radius = Float
 
 data Triangle = Triangle Point Point Point deriving (Show)
 
 data Square = Square Point Point Point Point deriving (Show)
 
+data Circle = Circle Point Radius deriving (Show)
+
 class Shape a where
-  rotate :: a -> a
   simple :: a
+  rotate :: a -> Maybe a
+  area :: a -> Maybe Float
+  (<^>) :: a -> Maybe a
 
 instance Shape Triangle where
-  rotate (Triangle x y z) = Triangle z x y
   simple = Triangle (0, 0) (1, 0) (0, 1)
+  rotate (Triangle x y z) = Just (Triangle z x y)
+  area (Triangle x y z) = Nothing
+  (<^>) (Triangle x y z) = Just (Triangle z x y)
+
+--area = (x1y2 + x2y3 + x3y1 – x1y3 – x2y1 – x3y2)/2.
 
 instance Shape Square where
-  rotate (Square w x y z) = Square z w x y
   simple = Square (0, 0) (1, 0) (1, 1) (0, 1)
+  rotate (Square w x y z) = Just (Square z w x y)
+  area (Square w x y z) = Nothing
+  (<^>) (Square w x y z) = Nothing
+
+instance Shape Circle where
+  simple = Circle (0, 0) 10
+  rotate (Circle x r) = Nothing
+  area (Circle x r) = Just (pi * r ^ 2)
+  (<^>) (Circle x r) = Nothing
