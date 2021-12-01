c67f8ef755e6bd0cd108dfdbd2e48041c5a1a8be
hoosain <hoosain.madhi@gmail.com>
2021-12-01 07:21:51 +0200

    numbertolcd

--- numberToLCD.hs
+++ numberToLCD.hs
@@ -11,6 +11,20 @@ Write a program that given a number (with arbitrary number of digits), converts 
   
 (each digit is 3 lines high)
 --------------------------------------------------------------------
+125
+line1 n _   _
+line2 | _| |_
+line3 ||_   _| 
+
+line1 " " "_" "_" " " "_" "_" "_" "_" "_"
+line2 "|" "_|" "_|" "|_|" "|_" "|_" "|" "|_|" "|_|"
+line3 "|" "|_" "_|" "s|" "_|" "|_|" "|" "|_|" "_|"
+
+example 158
+line1 " "  "_"   "_"
+line2 "|" "|_"  "|_|"
+line3 "|" " _|" "|_|"
+ 
          _______
         |       |           _
 2 ->    |       | ->        _|
@@ -31,18 +45,19 @@ digits 0 = []
 digits x = digits (x `div` 10) ++ [x `mod` 10]
 
 
-x = "This is some text which we escape \n\
-      \   and unescape to keep writing"
-
 
 digitToLCD :: Int -> String
 digitToLCD 1 = "|\n\
-               \|\n\
                \|\n"
+
 digitToLCD 2 = "_\n\
                \ _|\n\
                \|_\n"
-digitToLCD 3 = "Three"
+
+digitToLCD 3 = "_\n\
+               \ _|\n\
+               \ _|\n"
+
 digitToLCD 4 = "Four"
 digitToLCD 5 = "Five"
 digitToLCD 6 = "Six"
@@ -59,7 +74,8 @@ digitToLCD a = show a
 
 main :: IO ()
 main = do
-    let a = map digitToLCD (digits 121212222)
-    putStr(unwords a)
-    -- putStrLn (a !! 1)
+--     let a = map digitToLCD (digits 12)
+--     putStr(unwords a)
+--     print a
+       print [digitToLCD x | x <-digits 12]
     -- print "\n"
