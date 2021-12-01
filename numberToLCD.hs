40c9b040785f440b8cfa5fe0ea9ebefd8c69a444
Hoosain Madhi <hoosain.madhi@gmail.com>
2021-12-01 14:17:09 +0200

    impertive num2LCD

--- numberToLCD.hs
+++ numberToLCD.hs
@@ -16,21 +16,31 @@ line1 n _   _
 line2 | _| |_
 line3 ||_   _| 
 
-line1 " " "_" "_" " " "_" "_" "_" "_" "_"
-line2 "|" "_|" "_|" "|_|" "|_" "|_" "|" "|_|" "|_|"
-line3 "|" "|_" "_|" "s|" "_|" "|_|" "|" "|_|" "_|"
+line1 "  _  _     _  _  _  _  _ "
+line2 "| _| _||_||_ |_   ||_||_|"
+line3 "||_  _|  | _||_|  ||_| _|" 
 
 example 158
 line1 " "  "_"   "_"
 line2 "|" "|_"  "|_|"
 line3 "|" " _|" "|_|"
- 
+
          _______
         |       |           _
-2 ->    |       | ->        _|
+2 ->    |   f   | ->        _|
         |_______|          |_
-            
+              
+                       line1  _
+                       line2  _|   
+                       line3 |_
 
+l1 :: Int ->   String
+l2 :: Int ->   String
+l3 :: Int ->   String    
+
+print l1 
+print l2
+print l3
 --}
 
 
@@ -40,42 +50,89 @@ line3 "|" " _|" "|_|"
 
 import Data.List ()
 
+--             0   1  2   3   4   5   6   7   8   9
+--  line1 = "  _       _   _       _   _  _   _   _ "  
+--  line2 = " | |   |  _|  _| |_| |_  |_   | |_| |_|"  
+--  line3 = " |_|   | |_   _|   |  _| |_|  | |_|  _|"  
+--  
+--  line1 = " sus sss sus sus sss sus sus us sus sus"  
+--  line2 = " psp ssp sup sup pup pus pus sp pup pup"  
+--  line3 = " pup ssp pus sup ssp sup pup sp pup sup"  
+--
+--  line1 " "  "_"   "_"
+--  line2 "|" "|_"  "|_|"
+--  line3 "|" " _|" "|_|"
+
+sss = "   "  
+sus = " _ " 
+pss = "|  " 
+sup = " _|" 
+pup = "|_|" 
+pus = "|_ "
+ssp = "  |"
+uup = "__|"
+psp = "| |"
+sp = " |"
+
+
+l1 :: Int -> String 
+l1 1 = sss
+l1 2 = sus
+l1 3 = sus
+l1 4 = sss
+l1 5 = sus
+
+l2 :: Int -> String
+l2 1 = ssp
+l2 2 = sup
+l2 3 = sup
+l2 4 = pup
+l2 5 = pus
+
+l3 :: Int -> String
+l3 1 = ssp
+l3 2 = pus
+l3 3 = sup
+l3 4 = ssp
+l3 5 = sup
+
+displayLCD :: Int -> IO()
+displayLCD 1 = do
+    putStrLn (l1 1)
+    putStrLn (l2 1)
+    putStrLn (l3 1)
+displayLCD 2 = do
+    putStrLn (l1 2)
+    putStrLn (l2 2)
+    putStrLn (l3 2)
+
+
+
+
+
 digits :: Integral x => x -> [x]
 digits 0 = []
 digits x = digits (x `div` 10) ++ [x `mod` 10]
 
 
 
-digitToLCD :: Int -> String
-digitToLCD 1 = "|\n\
-               \|\n"
 
-digitToLCD 2 = "_\n\
-               \ _|\n\
-               \|_\n"
-
-digitToLCD 3 = "_\n\
-               \ _|\n\
-               \ _|\n"
-
-digitToLCD 4 = "Four"
-digitToLCD 5 = "Five"
-digitToLCD 6 = "Six"
-digitToLCD 7 = "Seven"
-digitToLCD 8 = "Eight"
-digitToLCD 9 = "Nine"
-digitToLCD 0 = "Zero"
-
-
-
-
-
-digitToLCD a = show a
-
-main :: IO ()
+-- main :: IO ()
 main = do
---     let a = map digitToLCD (digits 12)
---     putStr(unwords a)
---     print a
-       print [digitToLCD x | x <-digits 12]
-    -- print "\n"
+       
+    -- let a = map displayLCD (digits 12)
+    -- putStr(unwords a)
+    -- let line1 =  map l1 digits 12121212121212121 --[1,2,1,2,1,2]
+
+    -- let b = unwords a
+    let line1 = [l1 x | x<- digits 1234554321]
+    let line2 = [l2 x | x<- digits 1234554321]
+    let line3 = [l3 x | x<- digits 1234554321]
+    
+   
+    print (unwords line1)
+    print (unwords line2)
+    print (unwords line3)
+    
+    -- print (unwords line2)
+    -- print (unwords line3)
